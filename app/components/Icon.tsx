343e3b40cea2092d621795c78169c2c9709e80f4
ZacZhangzhuo <zac.zhangzhuo@gmail.com>
2026-04-20 23:27:39 +0200

    feat

--- app/components/Icon.tsx
+++ app/components/Icon.tsx
@@ -1,6 +1,15 @@
-import * as React from "react";
-import SvgIcon from "@mui/material/SvgIcon";
+import { useColorScheme } from "@mui/material/styles";
+
+function WhiteIcon() {
+	return <img src={"/media/icon_white.png"} alt={"acssz icon"} height={28} />;
+}
+
+function BlackIcon() {
+	return <img src={"/media/icon_black.png"} alt={"acssz icon"} height={28} />;
+}
 
 export default function Icon() {
-	return <img src={"/favicon.ico"} alt={"acssz icon"} width={19} height={25} />;
+	const { mode, systemMode } = useColorScheme();
+	const darkMode = ((systemMode || mode) as "light" | "dark") === "dark";
+	return darkMode ? <WhiteIcon /> : <BlackIcon />;
 }
